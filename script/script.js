a225775ed985a32f6f786af97cb24b3fdf4d6492
Pham Hoang Khoi <khoiphamkevin2004@gmail.com>
2022-11-15 15:34:33 -0800

    DOM activity 1 with radius, ID and MATH method

--- script/script.js
+++ script/script.js
@@ -14,11 +14,25 @@
 // greet("Khôi")
 
 
-const greet = (user,time = "day") => {   // # Method 3 to call a function
-    // console.log("Hello" +  user);
-    console.log(`Good ${time} ${user}`);
+// const greet = (user,time = "day") => {   // # Method 3 to call a function
+//     // console.log("Hello" +  user);
+//     console.log(`Good ${time} ${user}`);
+// }
+
+// greet("Khôi", "night")
+
+let r = prompt("Enter a radius.")
+console.log(r)
+
+function circleArea (radius){
+    const radiusP = document.querySelector("#radius"); 
+    const resultP = document.querySelector("#result");
+    radiusP.textContent = r
+    let calculatedArea = Math.PI * radius * radius;
+    resultP.textContent = calculatedArea.toFixed(2);
+    return calculatedArea;
 }
 
-greet("Khôi", "night")
+let area = circleArea(r).toFixed(2);
 
-fruits.pop(2)
+alert(`The area of a circle with radius ${r} is ${area}`)
